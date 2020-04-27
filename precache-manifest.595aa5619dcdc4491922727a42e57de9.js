f388848b885d0fd26f1da832b8c3862cba2b2522
FuzzyChen <cxwhyyjzn@163.com>
2020-04-26 22:26:30 -0400

    Updates

---
+++ precache-manifest.595aa5619dcdc4491922727a42e57de9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7704af3e68733db153705ac362637126",
+    "url": "/index.html"
+  },
+  {
+    "revision": "09c22a4a591e1ba04c84",
+    "url": "/static/css/2.8d06fd47.chunk.css"
+  },
+  {
+    "revision": "9955d7cd7d3f9bb462bb",
+    "url": "/static/css/main.da7aa426.chunk.css"
+  },
+  {
+    "revision": "09c22a4a591e1ba04c84",
+    "url": "/static/js/2.5efeeee2.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.5efeeee2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9955d7cd7d3f9bb462bb",
+    "url": "/static/js/main.f0a7072f.chunk.js"
+  },
+  {
+    "revision": "5284eec07972ace79c95",
+    "url": "/static/js/runtime-main.ceaf4b5f.js"
+  }
+]);
